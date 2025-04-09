f028f248f8810a18d43eb5efe4d0e102c87e0093
KamikazEr101 <1305990365@qq.com>
2025-04-09 14:50:26 +0800

    init1

--- pro_classify/src/main/java/com/xinghuo/pro_classify/config/SpringBootConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.xinghuo.pro_classify.config;
-
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
-
-@Component
-@EnableScheduling
-public class SpringBootConfiguration {
-}
